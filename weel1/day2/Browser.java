eec1994a4e3ac4ec2b91ac57ce36fbadfc94d58d
Veronicaloysius <veronicanepolean007@gmail.com>
2024-06-27 17:21:17 +0530

    Add files via upload

---
+++ weel1/day2/Browser.java
@@ -0,0 +1,36 @@
+package weel1.day2;
+
+public class Browser {
+
+	public String launchbrowser (String browserName ) {
+		System.out.println("Browser launch sucessfully");
+		
+		return browserName;
+		
+			}
+	public void loadurl() {
+		
+		System.out.println("Application loaded sucessfully");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
